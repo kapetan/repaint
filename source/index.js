2a6d2e3d0d4cbbc7e892fa2b5cad8e68e45d5857
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2015-04-05 21:15:17 +0200

    Return emitter

--- source/index.js
+++ source/index.js
@@ -1,5 +1,6 @@
+var events = require('events');
 var afterAll = require('after-all');
-var extend = require('xtend');
+var extend = require('xtend/mutable');
 
 var html = require('./html');
 var stylesheets = require('./stylesheets');
@@ -11,41 +12,70 @@ var Stylesheet = require('./css/stylesheet');
 
 var noop = function() {};
 
+var clone = function(obj) {
+	return JSON.parse(JSON.stringify(obj));
+};
+
 var defaultStylesheets = function(stylesheets) {
 	return (stylesheets || []).map(function(sheet, i) {
 		return Stylesheet.parse(sheet, i - stylesheets.length);
 	});
 };
 
-module.exports = function(page, callback) {
+module.exports = function(options, callback) {
 	callback = callback || noop;
 
-	var position = page.viewport.position;
-	page.viewport.position = extend({ x: 0, y: 0 }, position);
+	var page = new events.EventEmitter();
+	var emit = page.emit.bind(page);
+	var ondone = function(err) {
+		if(err) {
+			emit('fail', err);
+			return callback(err);
+		}
 
-	html(page.content, function(err, document) {
-		if(err) return callback(err);
+		emit('ready', page);
+		callback(null, page);
+	};
+
+	extend(page, options, {
+		stylesheets: null,
+		viewport: clone(options.viewport)
+	});
+
+	var position = options.viewport.position;
+	options.viewport.position = extend({ x: 0, y: 0 }, position);
+
+	html(options.content, function(err, document) {
+		if(err) return ondone(err);
+
+		page.document = document;
+		emit('html', document);
 
 		var next = afterAll(function(err) {
-			if(err) return callback(err);
+			if(err) return ondone(err);
 
-			var tree = layout(document.html, page.viewport);
-			draw(tree, page.context);
-
-			page.document = document;
+			var tree = layout(document.html, options.viewport);
 			page.layout = tree;
+			emit('layout', tree);
 
-			callback(null, page);
+			draw(tree, options.context);
+			emit('draw');
+
+			ondone();
 		});
 
-		images(page.url, document.images, next(function(err, images) {
+		images(options.url, document.images, next(function(err, images) {
 			page.images = images;
+			emit('images', images);
 		}));
 
-		stylesheets(page.url, document.stylesheets, next(function(err, stylesheets) {
-			stylesheets = defaultStylesheets(page.stylesheets).concat(stylesheets);
+		stylesheets(options.url, document.stylesheets, next(function(err, stylesheets) {
+			stylesheets = defaultStylesheets(options.stylesheets).concat(stylesheets);
 			css(stylesheets, document.html);
 			page.stylesheets = stylesheets;
+			emit('stylesheets', stylesheets);
 		}));
 	});
+
+	return page;
 };
